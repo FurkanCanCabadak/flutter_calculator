cf713020bc17e1c7a11a54d336c55a7f2286e97f
FURKAN_CABADAK <furkancancabadak@gmail.com>
2022-07-02 16:32:13 +0300

    Display issues fixed.

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_calculator/result_display.dart';
-import 'package:flutter_calculator/services.dart';
 import 'calc_button.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 // ignore: use_key_in_widget_constructors, must_be_immutable
 class MyHomePage extends StatefulWidget {
@@ -68,5 +68,32 @@ class _MyHomePageState extends State<MyHomePage> {
                 }))
       ],
     );
+  }
+
+  void buttonCheck(String btnTxt) {
+    if (btnTxt == 'C') {
+      userInput = '';
+      answer = '0';
+    } else if (btnTxt == '%') {
+    } else if (btnTxt == 'DEL') {
+      userInput = userInput.substring(0, userInput.length - 1);
+    } else if (btnTxt == '=') {
+      equalPressed();
+    } else {
+      userInput += btnTxt;
+    }
+  }
+
+// function to calculate the input operation
+  void equalPressed() {
+    String finaluserinput = userInput;
+    finaluserinput = userInput.replaceAll('x', '*');
+
+    Parser p = Parser();
+    Expression exp = p.parse(finaluserinput);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toString();
+    userInput = answer;
   }
 }
